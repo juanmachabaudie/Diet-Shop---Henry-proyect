0dc5dbb926c3af0cc21c69455568c764aa342b14
kevin <kevin.queiro@outlook.com>
2021-05-04 13:37:42 -0300

    preparing all
    armado de estructura de front

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -1,9 +1,16 @@
-import {createStore, applyMiddleware, compose} from 'redux'
-import rootReducer from '../reducers/reducer' //el reducer
+import {createStore, applyMiddleware, compose,combineReducers} from 'redux'
+// import reducer_1 from '../reducers/reducer' //el reducer
 import thunk from 'redux-thunk' //nos ayuda a trabajar con promesas con redux
 //import {composeWithDevTools} from 'redux-devtools-extension' //nos ayuda a ver los state de la herramienta
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+//aca va el objeto con los nombres de los reducers ej:
+// miReducer: reducer_1  (obiamente con la respectiva importacion del reducer_1)
+
+})
+
 
 export const store = createStore(
     rootReducer, // --->>  persistedReducer
