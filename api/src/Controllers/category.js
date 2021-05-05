48626fe6c2e46120090b48f3c134be7389bfc3f2
francisco garrido <fg0611@gmail.com>
2021-05-04 22:13:43 -0300

    v1.0 Model Category & Routes
    Model Category & Routes first commit

---
+++ api/src/Controllers/category.js
@@ -0,0 +1,74 @@
+const axios = require("axios");
+const fetch = require("node-fetch");
+const { uuid } = require("uuidv4");
+const { Category } = require("../db");
+const { Op } = require("sequelize");
+
+async function getAllCategories(req, res, next) {
+  try {
+    const categories = await Category.findAll();
+    console.log(categories);
+    return await res.send(categories);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function createCategory(req, res, next) {
+  try {
+    req.body.id = uuid();
+
+    const { name, id } = req.body;
+    //console.log("atts: ", id, name);
+    const category = await Category.create({
+      id: id,
+      name: name,
+    });
+    console.log(category);
+    return await res.send(category);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function deleteCategory(req, res, next) {
+  try {
+    const { id } = req.body;
+    const category = await Category.destroy({
+      where: {
+        id: id,
+      },
+    });
+    const categories = await Category.findAll();
+    console.log(categories);
+    return await res.send(categories);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function updateCategory(req, res, next) {
+  try {
+    const { id, name } = req.body;
+    const category = await Category.update(
+      { name: name },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+
+    console.log(category);
+    return await res.send(category);
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = {
+  getAllCategories,
+  createCategory,
+  deleteCategory,
+  updateCategory,
+};
--- api/src/db.js
+++ api/src/db.js
@@ -5,7 +5,7 @@ const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/development`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/diet`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
@@ -37,14 +37,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-
-const {} = sequelize.models;
-
-// Aca vendrian las relaciones
-// Product.hasMany(Reviews);
-
-User.hasOne(Cart);
-Cart.belongsTo(User);
+const { Category } = sequelize.models;
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
