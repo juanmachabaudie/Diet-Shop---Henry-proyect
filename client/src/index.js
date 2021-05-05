aef2c317814c0e2f30c8392c4f72b11e16d4b08b
Marcos Carrizo <marcoscarrizo7@gmail.com>
2021-05-05 14:24:00 -0300

    component

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import {store} from './redux/store/store';
 import {BrowserRouter} from 'react-router-dom';
+import App from './App'
+
 
 ReactDom.render(    
     <Provider store={store}>        
