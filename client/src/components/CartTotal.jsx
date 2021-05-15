e726277adceba5ae1dcb4b40c5f55d8cb0da281b
krono999 <you@example.com>
2021-05-15 18:44:16 -0300

    CartTotal

--- client/src/components/CartTotal.jsx
+++ client/src/components/CartTotal.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Typography, Button } from '@material-ui/core';
 
-let products = [{ price: 22, quantity: 2 }, { price: 33, quantity: 1 }]
+let products = [{ price: 333, quantity: 1 }, { price: 455, quantity: 2 }]
 
 const CartTotal = () => {
     // const dispatch = useDispatch();
@@ -19,7 +19,7 @@ const CartTotal = () => {
     return (
         <Container>
             
-            <Typography>Total: {total}</Typography>
+            <Typography>Total: {total} </Typography>
              <Button> Comprar </Button>
         </Container>
     );
