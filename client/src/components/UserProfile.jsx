5bb47c422a1b3d25965cd78e370f3ad4c2faf366
juanmachabaudie <jumachabaudie@gmail.com>
2021-05-27 23:59:01 -0300

    Update UserProfile.jsx

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -1,13 +1,6 @@
 import React from "react";
-import {
-  Box,
-  Button,
-  CardMedia,
-  Grid,
-  Typography,
-  makeStyles,
-} from "@material-ui/core";
-import jwt from 'jsonwebtoken';
+import { Box, Button, CardMedia, Grid, Typography, makeStyles } from "@material-ui/core";
+import jwt from "jsonwebtoken";
 
 const useStyles = makeStyles((theme) => ({
   description: {
@@ -24,24 +17,23 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const UserProfile = () => {
-  const token = sessionStorage.getItem('user');
-  const tokeen = JSON.parse(token)
-  const user = jwt.decode(tokeen)
+  const token = sessionStorage.getItem("user");
+  const tokeen = JSON.parse(token);
+  const user = jwt.decode(tokeen);
 
   const userAdmin = user.isAdmin;
   const classes = useStyles();
-
 
   return (
     <div>
       <Grid container>
         <Grid Item xs={12} md={12} justifyContent="center" alignItems="center">
-            <CardMedia
-              component="img"
-              className={classes.media}
-              src="https://www.labicok.com/wp-content/uploads/2020/09/default-user-image.png"
-            />
-            <Typography variant="h4">Bienvenidx {user.firstName}!</Typography>
+          <CardMedia
+            component="img"
+            className={classes.media}
+            src="https://avatars.githubusercontent.com/u/54723628?v=4"
+          />
+          <Typography variant="h4">Bienvenidx {user.firstName}!</Typography>
         </Grid>
         <Grid Item xs={6} md={4}>
           <Box m={6} display="flex" justifyContent="center">
@@ -64,32 +56,33 @@ const UserProfile = () => {
             </Button>
           </Box>
         </Grid>
-        {userAdmin?
-        <>
-        <Grid Item xs={6} md={4}>
-          <Box m={6} display="flex" justifyContent="center">
-            <Button href="/product/add" variant="contained" color="primary">
-              Agregar Productos
-            </Button>
-          </Box>
-        </Grid>
-        <Grid Item xs={6} md={4}>
-          <Box m={6} display="flex" justifyContent="center">
-            <Button href="/category/add" color="primary" variant="contained">
-              Agregar Categorias
-            </Button>
-          </Box>
-        </Grid>
-        <Grid Item xs={6} md={4}>
-          <Box m={6} display="flex" justifyContent="center">
-            <Button href="/user/promote" color="primary" variant="contained">
-              Asignar Rol
-            </Button>
-          </Box>
-        </Grid>
-        </>
-        :<> </>
-      }
+        {userAdmin ? (
+          <>
+            <Grid Item xs={6} md={4}>
+              <Box m={6} display="flex" justifyContent="center">
+                <Button href="/product/add" variant="contained" color="primary">
+                  Agregar Productos
+                </Button>
+              </Box>
+            </Grid>
+            <Grid Item xs={6} md={4}>
+              <Box m={6} display="flex" justifyContent="center">
+                <Button href="/category/add" color="primary" variant="contained">
+                  Agregar Categorias
+                </Button>
+              </Box>
+            </Grid>
+            <Grid Item xs={6} md={4}>
+              <Box m={6} display="flex" justifyContent="center">
+                <Button href="/user/promote" color="primary" variant="contained">
+                  Asignar Rol
+                </Button>
+              </Box>
+            </Grid>
+          </>
+        ) : (
+          <> </>
+        )}
       </Grid>
     </div>
   );
